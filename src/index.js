51c7ad7ba629bc2ddddea87b27ad4cf4e6e18a4d
Guadalupe Isidro (LÜ ANGELES) <51386526+LuAngeles@users.noreply.github.com>
2019-06-14 11:07:11 -0500

    Dejé las imágenes como estaban y modifiqué index html y js

--- src/index.js
+++ src/index.js
@@ -57,7 +57,8 @@ const mostrarSeccion=(id)=>{
   //Mandará llamar con el botón de "EMPLEADO" a la página de clave de usuario//
   empleadoButton.addEventListener('click',verPagEmpleado);
 
-  //aquí debo mandar abrir desde pág 2 a E1 //
+  //aquí debo mandar abrir desde pág 2 a E1 
+  // Le voy a solicitar al empleado que escriba su nombre y clave de usuario para enviarlo a la página donde escribirá el código encriptado//
   const enviarDatosEmpleado=document.getElementById('datos-empleado');
   const codigoEmpleado=()=>{
     ocultarSeccion('pag-empleado');
@@ -66,6 +67,7 @@ const mostrarSeccion=(id)=>{
   enviarDatosEmpleado.addEventListener('click',codigoEmpleado);
 
   //Mando de E1 a E2 de hoja de mi prototipo//
+  // Le voy a solicitar al empleado que escriba el código encriptado y así pueda enviar su trabajo//
   const enviarCodEncriptado=document.getElementById('codigo-encriptado');
   const codEncriptadoEmpleado=()=>{
     ocultarSeccion('codigo-empleado');
@@ -90,6 +92,27 @@ const mostrarSeccion=(id)=>{
   registroBoton=document.getElementById('click',pagRegistro)
   
   
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Esto fue en lo que avancé para realizar el cifrar y decifrar //
   
-  
+  let palabraEncriptada=''; // Aquí es un arreglo en el que se irán guardando la 'palabra' ya encriptada
+for(i=0;i<longitudPalabra;i++){
+let posicionLetra=(palabra.charCodeAt(i)); //Esta es la posición ASCII de cada letra ingresada 
+let positionAsciiLetra=(((posicionLetra-65)+offset)%26+65); //Solicito la nueva posición ASCII de letra por letra según el offset
+let letraEncriptada=String.fromCharCode(positionAsciiLetra); //Solicito que la posición anterior me la vuelvo caracter
+palabraEncriptada= palabraEncriptada + letraEncriptada;
+};
+
+console.log('Tu código fue la palabra: ' + palabra + ' con un desplazamiento de ' + offset + ',por lo que, tu código encriptado es: '+palabraEncriptada);
+
+let palDesencriptada='';
+for(i=0;i<palabraEncriptada.length;i++){;
+let posLetDesencri=(palabraEncriptada.charCodeAt(i));
+let posDesencri=(posLetDesencri-65-offset)+52%26+65;
+let letraDesencri=String.fromCharCode(posDesencri);
+palDesencriptada=palDesencriptada + letraDesencri;
+}
+console.log('Tu código encriptado es: ' + palabraEncriptada + ' con un desplazamiento de ' + offset + ',así que tu palabra original es '+ palDesencriptada);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
