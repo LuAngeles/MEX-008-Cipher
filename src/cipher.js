1204de2ce3130e0dbaf50031753bd877368627af
Guadalupe Isidro (LÜ ANGELES) <51386526+LuAngeles@users.noreply.github.com>
2019-06-18 15:18:11 -0500

    Modifiqué y agregué .code y .decode para mayúsculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,31 @@
 window.cipher = {
- //... 
- cifrar:('HOLA',2)
-
+    encode:(offset,palabra)=>{
+        palabra=palabra.toUpperCase();
+       
+        let palabraEncriptada=''; // Aquí es un arreglo que empieza vacío en el que se irá guardando la 'palabra' ya encriptada
+  
+        for(let i=0;i<palabra.length;i++){
+            let posicionLetra=(palabra.charCodeAt(i)); //Esta es la posición ASCII de cada letra ingresada 
+            let positionAsciiLetra=(((posicionLetra-65)+offset)%26+65); //Solicito la nueva posición ASCII de letra por letra según el offset
+            let letraEncriptada=String.fromCharCode(positionAsciiLetra); //Solicito que la posición anterior me la vuelvo caracter
+            palabraEncriptada= palabraEncriptada + letraEncriptada;
+            }
+            return palabraEncriptada;
+        },
+    decode:(offset,palabraEncriptada)=>{
+        palabraEncriptada=palabraEncriptada.toUpperCase();
+        
+        let palDesencriptada=''; //Arreglo vacío en el que se irá guardando la palabra a descencriptar
+        
+        for(let i=0;i<palabraEncriptada.length;i++){
+            let posLetDesencri=(palabraEncriptada.charCodeAt(i)); //Solicito la posición ASCII de cada letra encriptada
+            let posDesencri=(posLetDesencri-65-offset+52)%26+65; //Una vez que me dio la posición anterior, quiero que me de la posición a la cual debe regresar originalmente 
+            let letraDesencri=String.fromCharCode(posDesencri); //Solicitio que el valor obtenido que lo convierta a caracter
+            palDesencriptada=palDesencriptada + letraDesencri;
+            }
+            return palDesencriptada;
+        }
 };
+//module.exports=cipher;
+
 
--- src/index.js
+++ src/index.js
@@ -1,121 +1,158 @@
 //Variables generales para mostrar y ocultar secciones -esto evita abrir varios HTML's//
 const mostrarSeccion=(id)=>{
-    document.getElementById(id).classList.remove('oculto')
+  document.getElementById(id).classList.remove('oculto')
   }
-  const ocultarSeccion=(id)=>{
-    document.getElementById(id).classList.add('oculto')
+const ocultarSeccion=(id)=>{
+  document.getElementById(id).classList.add('oculto')
   }
   
   const ocultarBoton=(id)=>{
     document.getElementById(id).classList.add('boton')
   }
     
-  ////////////////////////////////////////////////////////////////////
-   ////// DE HOJA 1 A HOJA 1.1 U HOJA 2 /////////////////////////////
+////////////////////////////////////////////////////////////////////
+ 
+//////// SECCIÓN DEL JEFE /////////////////////////
   
-  //////// JEFE /////////////////////////
+//// PROTOTIPO DE HOJA 1 A HOJA 1.1 /////////
+//Variable para el botón que el usuario presionará si es jefe (prototipo hoja 1)
+const jefeButton=document.getElementById('jefe-boton');
   
-  ////////// PROTOTIPO DE HOJA 1 A HOJA 1.1 /////////
-  //Variable para el botón que el usuario presionará si es jefe (prototipo hoja 1)
-  const jefeButton=document.getElementById('jefe-boton');
-  
-  //Variable en la que abrirá una nueva pagina, una vez que, dieron click en "jefe" (prototipo hoja 1.1)
-  const verPagJefe=()=>{
-    ocultarSeccion('TipoDeUsuario');
-    ocultarBoton('registro-boton');
-    mostrarSeccion('pag-jefe');
+//Variable en la que abrirá una nueva pagina, una vez que, dieron click en "jefe" (prototipo hoja 1.1)
+const verPagJefe=()=>{
+  ocultarSeccion('TipoDeUsuario');
+  ocultarBoton('registro-boton');
+  mostrarSeccion('pag-jefe');
   }
-  //Mandará llamar con el botón de "jefe" a la página de clave de usuario//
-  jefeButton.addEventListener('click',verPagJefe);
-  
-  /////// PROTOTIPO DE HOJA 1.1 A HOJA 3 ///////////////////////////
-  const enviarClUsuario=document.getElementById('enviar-clave-usuario');
-  
-  const verPagCod=()=>{
-    ocultarSeccion('registro-boton');
-    ocultarSeccion('pag-jefe');
-    mostrarSeccion("pag-crear-codigo");
-  }
-    enviarClUsuario.addEventListener('click',verPagCod);
+ //Mandará llamar con el botón de "jefe" a la página de clave de usuario//
+jefeButton.addEventListener('click',verPagJefe);
 
-    //// PROTOTIPO DE HOJA 3 A HOJA 4 ////
-    const codEncriptadoJefe=document.getElementById('encriptar');
-    const verPagCodEncriptado=()=>{
-      ocultarSeccion('pag-crear-codigo');
-      mostrarSeccion('codigo-final');
+/// Sección de Instrucciones en hoja de portada ///
+  ///PARA ABRIR LA PÁGINA///
+const instrucciones=document.getElementById('boton-1');
+const cerrar=document.getElementById('cerrar');
+
+const pagInstrucciones=()=>{
+  mostrarSeccion('instrucciones-1');
+  }
+instrucciones.addEventListener('click',pagInstrucciones);
+
+/// CERRAR PÁGINA ///
+const cerrarPag=()=>{
+  ocultarSeccion('instrucciones-1');
+}
+cerrar.addEventListener('click',cerrarPag);
+
+//// PROTOTIPO DE HOJA 1.1 A HOJA 3 ///////////////////////////
+const enviarClUsuario=document.getElementById('enviar-clave-usuario');
+  
+const verPagCod=()=>{
+  ocultarSeccion('registro-boton');
+  ocultarSeccion('pag-jefe');
+  mostrarSeccion("pag-crear-codigo");
+  }
+enviarClUsuario.addEventListener('click',verPagCod);
+
+//// PROTOTIPO DE HOJA 3 A HOJA 4 ////
+////Página que aparecerá para CIFRAR////
+const desplazamiento=document.getElementById('desplazamiento');
+const palabraOriginal=document.getElementById('crear-codigo-para-empleado');
+
+
+const codEncriptadoJefe=document.getElementById('encriptar');
+
+const verPagCodEncriptado=()=>{
+  let valorDesplazamiento=parseInt(desplazamiento.value); //cambia a número//
+  const codigoCifrado=document.getElementById('codigo-cifrado');
+  let codigoFinal=cipher.encode(valorDesplazamiento,palabraOriginal.value);
+  codigoCifrado.innerHTML=codigoFinal;
+    
+  ocultarSeccion('pag-crear-codigo');
+  mostrarSeccion('codigo-final');
+  }
+codEncriptadoJefe.addEventListener('click',verPagCodEncriptado);
+  
+/// Sección de instrucciones en hoja 3 de prototipo ////
+  /// ABRIR PÁGINA///
+const instruc2=document.getElementById('boton-2');
+    const PagInstruc2=()=>{
+      mostrarSeccion('instrucciones-2');
+      //mostrarSeccion('cerrar2');
     }
-    codEncriptadoJefe.addEventListener('click',verPagCodEncriptado);
-  
-  
-  ///////// EMPLEADO ////////////////////////
-  //Variable para el botón que el usuario presionará si es "EMPLEADO" (Prototipo hoja 1)
-  const empleadoButton=document.getElementById('empleado-boton');
-  const verPagEmpleado=()=>{
-    ocultarSeccion('TipoDeUsuario');
-    mostrarSeccion('pag-empleado');
-  }
-  //Mandará llamar con el botón de "EMPLEADO" a la página de clave de usuario//
-  empleadoButton.addEventListener('click',verPagEmpleado);
+    instruc2.addEventListener('click',PagInstruc2);
 
-  //aquí debo mandar abrir desde pág 2 a E1 
-  // Le voy a solicitar al empleado que escriba su nombre y clave de usuario para enviarlo a la página donde escribirá el código encriptado//
-  const enviarDatosEmpleado=document.getElementById('datos-empleado');
-  const codigoEmpleado=()=>{
-    ocultarSeccion('pag-empleado');
-    mostrarSeccion('codigo-empleado');
+  /// CERRAR PÁGINA ///
+const botoncerrar=document.getElementById('cerrar2');
+const cierrePag=()=>{
+  ocultarSeccion('instrucciones-2');
   }
-  enviarDatosEmpleado.addEventListener('click',codigoEmpleado);
+botoncerrar.addEventListener('click',cierrePag);
 
-  //Mando de E1 a E2 de hoja de mi prototipo//
-  // Le voy a solicitar al empleado que escriba el código encriptado y así pueda enviar su trabajo//
-  const enviarCodEncriptado=document.getElementById('codigo-encriptado');
-  const codEncriptadoEmpleado=()=>{
-    ocultarSeccion('codigo-empleado');
-    mostrarSeccion('enviar-trabajo');
+//// HOJA 4.1 DE PROTOTIPO ////
+/// Página que aparecerá para desencriptar o recuperar la contraseña original///
+ const paginaDesencr=document.getElementById('decode-codigo');
+ 
+ const verPaginaDesencr=()=>{
+  ocultarSeccion('codigo-final');
+  mostrarSeccion('recuperar-codigo');
+}
+paginaDesencr.addEventListener('click',verPaginaDesencr);
+
+//// HOJA 4.2 DE PROTOTIPO ////
+
+const desplEncrip=document.getElementById('desplazamiento-encriptado');
+const palabraEncriptada=document.getElementById('code-encript');
+const codDesencriptado=document.getElementById('desencriptar');
+//const desencrFinal=document.getElementById('desencriptar')
+
+const verPagDesencr=()=>{
+  let valDesplEncr=parseInt(desplEncrip.value);
+  const codigoDesencriptado=document.getElementById('codigo-decifrado');
+  let codDesFinal=cipher.decode(valDesplEncr,palabraEncriptada.value);
+  codigoDesencriptado.innerHTML=codDesFinal;
+
+  ocultarSeccion('recuperar-codigo');
+  mostrarSeccion('code-desencriptado');
   }
-  enviarCodEncriptado.addEventListener('click',codEncriptadoEmpleado);
+codDesencriptado.addEventListener('click',verPagDesencr);
+
+//////////////////////////////////////////////////////////////////////////////////////////
+///////// SECCIÓN PARA EL EMPLEADO ////////////////////////
+//Variable para el botón que el usuario presionará si es "EMPLEADO" (Prototipo hoja 1)
+const empleadoButton=document.getElementById('empleado-boton');
+const verPagEmpleado=()=>{
+  ocultarSeccion('TipoDeUsuario');
+  mostrarSeccion('pag-empleado');
+  }
+//Mandará llamar con el botón de "EMPLEADO" a la página de clave de usuario//
+empleadoButton.addEventListener('click',verPagEmpleado);
+
+//aquí debo mandar abrir desde pág 2 a E1 
+// Le voy a solicitar al empleado que escriba su nombre y clave de usuario para enviarlo a la página donde escribirá el código encriptado//
+const enviarDatosEmpleado=document.getElementById('datos-empleado');
+const codigoEmpleado=()=>{
+  ocultarSeccion('pag-empleado');
+  mostrarSeccion('codigo-empleado');
+  }
+enviarDatosEmpleado.addEventListener('click',codigoEmpleado);
+
+///Mando de E1 a E2 de hoja de mi prototipo//
+// Le voy a solicitar al empleado que escriba el código encriptado y así pueda enviar su trabajo//
+const enviarCodEncriptado=document.getElementById('codigo-encriptado');
+const codEncriptadoEmpleado=()=>{
+  ocultarSeccion('codigo-empleado');
+  mostrarSeccion('enviar-trabajo');
+  }
+enviarCodEncriptado.addEventListener('click',codEncriptadoEmpleado);
   
-  ////////////// REGISTRO //////////////////////
+////////////// REGISTRO //////////////////////
   
-  //Variable del botón que el usuario presionará si es nuevo (prototipo hoja 1)
-  const registroBoton=document.getElementById('registro-boton');
-  
+//Variable del botón que el usuario presionará si es nuevo (prototipo hoja 1)
+const registroBoton=document.getElementById('registro-boton');
   
 //Variable en la que abrirá una nueva pagina, una vez que, dieron click en "registro"(prototipo hoja 1.1)
 //Debe mostrar la misma que para ingresar como jefe//
-  registroBoton.addEventListener('click',verPagJefe);
+registroBoton.addEventListener('click',verPagJefe);
   
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-const cifrar=(palabra,offset)=>{
-  palabra=palabra.toUpperCase();
-  const longitudPalabra=palabra.length;
-  let palabraEncriptada=''; // Aquí es un arreglo que empieza vacío en el que se irá guardando la 'palabra' ya encriptada
-  
-  for(i=0;i<longitudPalabra;i++){
-  let posicionLetra=(palabra.charCodeAt(i)); //Esta es la posición ASCII de cada letra ingresada 
-  let positionAsciiLetra=(((posicionLetra-65)+offset)%26+65); //Solicito la nueva posición ASCII de letra por letra según el offset
-  let letraEncriptada=String.fromCharCode(positionAsciiLetra); //Solicito que la posición anterior me la vuelvo caracter
-  palabraEncriptada= palabraEncriptada + letraEncriptada;
-
-}
-//console.log('Tu código fue la palabra: ' + palabra + ' con un desplazamiento de ' + offset + ',por lo que, tu código encriptado es: '+palabraEncriptada);
-return(cifrar);
-};
-//cifrar('HOLA',2);
-
-
-
-const decifrar=(palabraEncriptada,offset)=>{
-let palDesencriptada=''; //Arreglo vacío en el que se irá guardando la palabra a descencriptar
-for(i=0;i<palabraEncriptada.length;i++){;
-let posLetDesencri=(palabraEncriptada.charCodeAt(i)); //Solicito la posición ASCII de cada letra encriptada
-let posDesencri=(posLetDesencri-65-offset)+52%26+65; //Una vez que me dio la posición anterior, quiero que me de la posición a la cual debe regresar originalmente 
-let letraDesencri=String.fromCharCode(posDesencri); //Solicitio que el valor obtenido que lo convierta a caracter
-palDesencriptada=palDesencriptada + letraDesencri;
-}
-//console.log('Tu código encriptado es: ' + palabraEncriptada + ' con un desplazamiento de ' + offset + ',así que tu palabra original es '+ palDesencriptada);
-return(decifrar);
-}
-//decifrar('JQNC',2);
 
